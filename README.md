94e867050ef4b3405893e07b9cc44bcb238fee7a
Sushil Nayak <sushil.nayak@gmail.com>
2023-07-09 12:42:33 +0530

    added sql scripts

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Sample GoLang Microservice
 - Gorilla Mux
 - sqlx + postgres
 - uber zap
+- uber mockgen [ go install go.uber.org/mock/mockgen@latest ]
---
+++ resources/database.sql
@@ -0,0 +1,15 @@
+create table students
+(
+    id   int,
+    name varchar(50),
+    age  int,
+    primary key (id)
+);
+
+insert into students(id, name, age)
+values (1, 'Sushil', 30);
+insert into students(id, name, age)
+values (2, 'John', 31);
+insert into students(id, name, age)
+values (3, 'Doe', 29);
+
